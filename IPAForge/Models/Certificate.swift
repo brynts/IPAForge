b8a1e15a59682fc7c31f61607c86be2519cbd6a5
Bryan <bryananta@icloud.com>
2026-08-25 17:08:39 +0700

    Expand Certificate model with PPQ, creation, appIDName

--- IPAForge/Models/Certificate.swift
+++ IPAForge/Models/Certificate.swift
@@ -4,20 +4,29 @@ struct Certificate: Identifiable, Codable, Hashable {
     let id: UUID
     var name: String
     
-    /// Folder name inside Documents/Certificates/{id}/
     var p12FileName: String
     var mobileProvisionFileName: String
     
     var password: String?
     var teamName: String?
     var teamIdentifier: String?
+    var appIDName: String?
+    var provisionName: String?
+    var creationDate: Date?
     var expirationDate: Date?
+    var ppqCheck: Bool?
     var isSelected: Bool
     var createdAt: Date
     
     var isExpired: Bool {
         guard let expirationDate else { return false }
         return expirationDate < Date()
+    }
+    
+    /// Days remaining until expiry (negative if expired).
+    var daysRemaining: Int? {
+        guard let expirationDate else { return nil }
+        return Calendar.current.dateComponents([.day], from: Date(), to: expirationDate).day
     }
     
     init(
@@ -28,7 +37,11 @@ struct Certificate: Identifiable, Codable, Hashable {
         password: String? = nil,
         teamName: String? = nil,
         teamIdentifier: String? = nil,
+        appIDName: String? = nil,
+        provisionName: String? = nil,
+        creationDate: Date? = nil,
         expirationDate: Date? = nil,
+        ppqCheck: Bool? = nil,
         isSelected: Bool = false,
         createdAt: Date = Date()
     ) {
@@ -39,7 +52,11 @@ struct Certificate: Identifiable, Codable, Hashable {
         self.password = password
         self.teamName = teamName
         self.teamIdentifier = teamIdentifier
+        self.appIDName = appIDName
+        self.provisionName = provisionName
+        self.creationDate = creationDate
         self.expirationDate = expirationDate
+        self.ppqCheck = ppqCheck
         self.isSelected = isSelected
         self.createdAt = createdAt
     }
